6b9de3a858a6ccb677832862c59663744c043880
Korjubzot <korjubzot@gmail.com>
2023-05-17 10:06:49 +0100

    Added growUp function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,18 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.name).toEqual('Fido');
+      });
+
+    it('sets the initial age as 1', () => {
+      const pet = new Pet('Fido');
+
+      pet.growUp();
+
+      expect(pet.age).toEqual(1);
+    });
   });
